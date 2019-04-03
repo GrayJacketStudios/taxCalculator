e364407ff752e28bc644a6543a1b31494f0f448a
Sebastian <grayjacketstudios@gmail.com>
2019-04-03 14:20:38 -0300

    Updates

---
+++ precache-manifest.4d334227b991d73123ed760767ff3288.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/taxCalculator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5c23185184d65624aa98",
+    "url": "/taxCalculator/static/js/runtime~main.ddc310d8.js"
+  },
+  {
+    "revision": "7420038c60c0fe16e349",
+    "url": "/taxCalculator/static/js/main.1b8b4ffd.chunk.js"
+  },
+  {
+    "revision": "9b3a5007987034dff77a",
+    "url": "/taxCalculator/static/js/2.652dea17.chunk.js"
+  },
+  {
+    "revision": "7420038c60c0fe16e349",
+    "url": "/taxCalculator/static/css/main.d8daadc9.chunk.css"
+  },
+  {
+    "revision": "b879960e8e516f1bad70698e43bbfdf3",
+    "url": "/taxCalculator/index.html"
+  }
+];
